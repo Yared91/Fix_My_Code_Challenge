ab3bc376a4328b8e607d5fbd18afd9c6b8f2e1c7
Yared91 <ykebede010@gmail.com>
2024-04-10 22:55:38 -0700

    update

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Index view
 """
-from flask import jsonify
+from flask import jsonify, Flask
 from api.v1.views import app_views
 
 
@@ -10,3 +10,8 @@ def status():
     """ Status of the web server
     """
     return jsonify({"status": "OK"})
+
+
+if __name__ == "__main__":
+    app.register_blueprint(app_views)
+    app.run(host='0.0.0.0', port=5000)
