c78ba0a60a08c0546134aecf81d33f44e8ed6f0c
Yared91 <ykebede010@gmail.com>
2024-04-11 06:14:05 -0700

    update

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -12,9 +12,9 @@ app.register_blueprint(app_views)
 @app.errorhandler(404)
 def not_found(error):
     """ json 404 page """
-    return make_response(jsonify({"Error": "Not found"}), 404)
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    """python -m api.v1.app"""
+    # python -m api.v1.app 
     app.run(host="0.0.0.0", port=5000)
--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """ Index view
 """
-from flask import jsonify, Flask
+from flask import jsonify
+
 from api.v1.views import app_views
 
 
-@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of the web server
     """
     return jsonify({"status": "OK"})
-
-
-if __name__ == "__main__":
-    app.register_blueprint(app_views)
-    app.run(host='0.0.0.0', port=5000)
